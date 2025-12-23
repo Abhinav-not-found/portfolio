5475b3364768a0afe4d287229cb059a4fe9a57ba
abhinav <abhinavisgreat18@gmail.com>
2025-12-23 23:48:28 +0530

    added techstack-section

--- src/components/sections/techStack.jsx
+++ src/components/sections/techStack.jsx
@@ -1,5 +1,10 @@
+"use client"
 import React from "react"
 import { BigHeading, Heading, SmallHeading } from "../general/heading"
+import Image from "next/image"
+import { techStack } from "@/lib/misc"
+
+// add tooltip
 
 const TechStack = () => {
   return (
@@ -8,10 +13,21 @@ const TechStack = () => {
         <SmallHeading>Tech</SmallHeading>
         <BigHeading>Stack</BigHeading>
       </Heading>
-      <div>
-        
+
+      <div className='grid grid-cols-7 gap-6 mt-6'>
+        {techStack.map((tech) => (
+          <Item key={tech.name} {...tech} />
+        ))}
       </div>
     </section>
+  )
+}
+
+const Item = ({ name, src }) => {
+  return (
+    <div className='flex flex-col items-center gap-2'>
+      <Image src={src} alt={name} width={40} height={40} priority={false} />
+    </div>
   )
 }
 
--- src/lib/misc.js
+++ src/lib/misc.js
@@ -27,3 +27,14 @@ export const socials = [
       label: "Email",
     },
   ]
+
+
+  export const techStack = [
+      { name: "MongoDB", src: "/images/mongodb.png" },
+      { name: "Express", src: "/images/express.png" },
+      { name: "React", src: "/images/react.png" },
+      { name: "Node.js", src: "/images/nodejs.png" },
+      { name: "Next.js", src: "/images/nextjs.png" },
+      { name: "Tailwind CSS", src: "/images/tailwind.png" },
+      { name: "Python", src: "/images/python.png" },
+    ]
