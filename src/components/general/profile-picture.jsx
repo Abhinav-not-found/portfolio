f112a4dce480337498d7fedbe0ad089452ca9cea
abhinav <abhinavisgreat18@gmail.com>
2026-05-07 12:25:03 +0530

    updated profile picture loading speed

--- src/components/general/profile-picture.jsx
+++ src/components/general/profile-picture.jsx
@@ -1,24 +1,24 @@
 "use client"
 
 import Image from "next/image"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 // make the picture background change in dark mode
 // add sound of camera when clicked
 
 const ProfilePic = () => {
   const profilePictures = [
-    "profile",
-    "profile2",
-    "profile3",
-    "profile4",
-    "profile5",
-    "profile6",
-    "profile7",
-    "profile8",
-    "profile9",
-    "profile10",
-    "profile11",
+    "https://ik.imagekit.io/nya4chyes/Portfolio/Profile%20Picture/profile.jpg",
+    "https://ik.imagekit.io/nya4chyes/Portfolio/Profile%20Picture/profile2.jpg",
+    "https://ik.imagekit.io/nya4chyes/Portfolio/Profile%20Picture/profile3.jpg",
+    "https://ik.imagekit.io/nya4chyes/Portfolio/Profile%20Picture/profile4.jpg",
+    "https://ik.imagekit.io/nya4chyes/Portfolio/Profile%20Picture/profile5.jpg",
+    "https://ik.imagekit.io/nya4chyes/Portfolio/Profile%20Picture/profile6.jpg",
+    "https://ik.imagekit.io/nya4chyes/Portfolio/Profile%20Picture/profile7.jpg",
+    "https://ik.imagekit.io/nya4chyes/Portfolio/Profile%20Picture/profile8.jpg",
+    "https://ik.imagekit.io/nya4chyes/Portfolio/Profile%20Picture/profile9.jpg",
+    "https://ik.imagekit.io/nya4chyes/Portfolio/Profile%20Picture/profile10.jpg",
+    "https://ik.imagekit.io/nya4chyes/Portfolio/Profile%20Picture/profile11.jpg",
   ]
 
   const [index, setIndex] = useState(0)
@@ -27,13 +27,21 @@ const ProfilePic = () => {
     setIndex((prev) => (prev + 1) % profilePictures.length)
   }
 
+  // pre-load the pictures
+  useEffect(() => {
+    profilePictures.forEach((pic) => {
+      const img = new window.Image()
+      img.src = pic
+    })
+  }, [])
+
   return (
     <div
       className='mt-10 cursor-pointer inline-block z-50'
       onClick={handleClick}
     >
       <Image
-        src={`/images/profile/${profilePictures[index]}.jpg`}
+        src={profilePictures[index]}
         alt='profile_pic'
         width={130}
         height={130}
